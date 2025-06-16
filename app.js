51d7bf7d80c4664e1ebde15ba68f26019ad2ca12
Deepak Sah <sahdeepak1199@gmail.com>
2025-06-17 00:06:38 +0530

    add project files

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
+
 const flash = require("express-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -18,8 +20,9 @@ const User = require("./models/user.js");
 const listingsRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
+const { error } = require("console");
 
-// const MONGOOSE_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
 const dbUrl = process.env.ATLASDB_URL;  
 
 main()
@@ -42,9 +45,21 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+    console.log("ErROR IN MOGO SESSION STORE", error);
+});
 
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -53,6 +68,8 @@ const sessionOptions = {
         httpOnly: true,
     }
 }
+
+
 
 
 app.use(session(sessionOptions));
